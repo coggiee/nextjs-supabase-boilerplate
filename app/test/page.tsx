4ef89efdf1ea42271e106d0827ea33970b8272a5
coggiee <zentechie7@gmail.com>
2024-12-02 13:53:26 +0900

    test: CustomImage 컴포넌트 내장 이미지 테스트

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 
 import { CustomImage } from "@/components/custom-image";
 
 import TestPng from "../../public/landing-object.webp";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
+  const [url, setUrl] = useState("./public/landing-object.webp");
+
+  const handleToggle = () => {
+    setUrl(
+      url === "./public/landing-object.webp"
+        ? "./public/test.png"
+        : "./public/landing-object.webp",
+    );
+  };
+
   return (
     <div className="flex h-full w-full items-center justify-center gap-4">
       <aside className="relative aspect-square w-[400px] overflow-hidden">
@@ -18,13 +31,14 @@ export default function Page() {
         />
       </aside>
       <CustomImage
-        src={"https://picsum.photos/200"}
+        src={url}
         alt="Test Image"
         width={400}
         height={400}
         quality={100}
         className="rounded-lg"
       />
+      <Button onClick={handleToggle}>Toggle Image</Button>
     </div>
   );
 }
